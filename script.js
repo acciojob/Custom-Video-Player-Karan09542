18bb4c4d6015a8df6dd7182b38438b08ce12d093
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-09-13 17:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay(){
+	if(video.paused){
+		video.play()
+		toggle.textContent = "❚ ❚"
+	}else{
+		video.pause()
+		toggle.textContent = "►"
+	} 
+}
+toggle.addEventListener("click", togglePlay)
+video.addEventListener("timeupdate",()=>{
+	const percent = (video.currentTime/ video.duration)*100;
+	progressBar.style.flexBasis = `${percent}%`
+})
+Array.from(ranges).forEach(range => {
+	range.addEventListener("input", (ele)=>{
+		if(ele.target.name === "volume"){
+			video.volume = parseFloat(range.value)
+		}else if( ele.target.name === "playbackRate"){
+			video.playbackRate = parseFloat(range.value)
+		}
+	})
+})
+Array.from(skipButtons).forEach(buttons => {
+	buttons.addEventListener("click", (button)=>{
+		console.log(button.target.dataset)
+		const skip = button.target.dataset.skip;
+		video.currentTime += parseFloat(skip)
+	})
+})
